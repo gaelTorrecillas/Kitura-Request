1300331ae9115b162cdbecda3173e0bf527d2449
gael.torrecillas <gael.torrecillas@imerir.com>
2016-11-17 15:29:33 +0100

    resolve error BodyPart.MimeType

--- Sources/KituraRequest/Encoding/MultipartFormData/BodyPart+MimeTypeSubtypes.swift
+++ Sources/KituraRequest/Encoding/MultipartFormData/BodyPart+MimeTypeSubtypes.swift
@@ -14,6 +14,47 @@
  * limitations under the License.
  **/
 
+import Foundation
+
+extension BodyPart {
+
+    /// Body Part mime type enumeration.
+    public enum MimeType {
+
+        /// Empty mime type.
+        case none
+
+        /// Text mime type.
+        case text(TextSubtype)
+
+        /// Image mime type.
+        case image(ImageSubtype)
+
+        /// Application mime type.
+        case application(ApplicationSubtype)
+
+        /// Custom mime type.
+        case raw(String)
+
+        /// String representation of mime type.
+        var value: String? {
+            switch self {
+            case .image(let type):
+                return "image/\(type.rawValue)"
+            case .text(let type):
+                return "text/\(type.rawValue)"
+            case .application(let type):
+                return "application/\(type.rawValue)"
+            case .raw(let value):
+                return value
+            default:
+                break
+            }
+            return nil
+        }
+    }
+}
+
 extension BodyPart.MimeType {
 
     /// Image media type.
